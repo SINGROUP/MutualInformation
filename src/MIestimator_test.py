87f4b76e7632d5455140eb915077041f160c1abd
Reisch Tobias <tobias.reisch@aalto.fi>
2017-07-20 15:32:47 +0300

    Added .gitignore

--- src/MIestimator_test.py
+++ src/MIestimator_test.py
@@ -1,6 +1,7 @@
 __author__ = 'binzam1'
 import numpy as np
 from pyMIestimator import pyMIestimator
+import matplotlib.pyplot as plt
 
 # test case:
 k=5
@@ -10,12 +11,15 @@ X=np.transpose(X)
 X=np.reshape(X, (N,1))
 Y=np.sin(X)
 
-#plt.figure(2)
-#plt.plot(X, Y, 'bo')  # plo
-#plt.grid()
-#plt.show()
+# plt.figure(2)
+# plt.plot(X, Y, 'bo')  # plo
+# plt.grid()
+# plt.show()
+
+X = np.load('../useMI/view_prediction_21014337.npy')
+Y = np.load('../useMI/view_solution_21014337.npy')
 
 k=5
-
+print X.shape
 [I1,I2]=pyMIestimator(X,Y,k)
 print I1, I2
