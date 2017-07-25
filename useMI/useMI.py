169bab5994d326509a27cdc38c735b6a3be3e5e3
Reisch Tobias <tobias.reisch@aalto.fi>
2017-07-25 13:27:22 +0300

    First commit???

--- useMI/useMI.py
+++ useMI/useMI.py
@@ -1,3 +1,20 @@
-    I=pyMIestimator(X,Y,k)
-    with open('Mutual_information-k'+str(k)+'.dat','a') as f:
-        f.write('I('+arg[0]+';'+arg[1]+') = '+str(I)+(' '+arg[2] if len(arg)>2 else '')+'\n')
+from MutualInformation import pyMIestimator
+import sys
+sys.path.append('/u/58/reischt1/unix/ml_projects/readAFM/src')
+import readAFMHDF5 as rHDF5
+
+
+db = rHDF5.AFMdata('/l/reischt1/toyDB_v14_twoAtoms3D.hdf5', [41,41,41,1])
+
+k = 5
+
+for sz in range(10, 30, 5):
+    for amp in [7.0, 10.0, 15.0]:
+        data = db.batch_runtimeSolution(200, sigmabasez = float(sz), amplificationFactor = amp, verbose = False)
+        X = data['forces']
+        Y = data['solutions']
+        
+        I=pyMIestimator(X,Y,k)
+        print('I(sz= '+str(sz)+'; amp= '+str(amp)+') = '+str(I))
+        with open('Mutual_information-toyDB_v14-k'+str(k)+'.dat','a') as f:
+            f.write('I(sz= '+str(sz)+'; amp= '+str(amp)+') = '+str(I)+'\n')
