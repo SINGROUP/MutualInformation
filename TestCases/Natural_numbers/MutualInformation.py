4b91f12bd742978c2bf3b9626f57e87248e4183e
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2017-08-07 13:54:02 +0300

    Merged pyMIestimator.py with MutualInformation.py, to avoid confusion Added TestCases

---
+++ TestCases/Natural_numbers/MutualInformation.py
@@ -0,0 +1,1 @@
+../../MutualInformation.py
---
+++ TestCases/Natural_numbers/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./MutualInformation.py Natural_numbers-500.npy Natural_numbers_squared-500.npy 500\ natural\ numbers
+./MutualInformation.py Natural_numbers-1000.npy Natural_numbers_squared-1000.npy 1000\ natural\ numbers
