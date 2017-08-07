5fe4b41c3f258bc2df88531e9a599f3777a7c41b
Reisch Tobias <tobias.reisch@aalto.fi>
2017-08-07 10:55:45 +0300

    useMI.py for my purposes

--- useMI/useMI.py
+++ useMI/useMI.py
@@ -1,20 +1,34 @@
-from MutualInformation import pyMIestimator
+'''
+Created on Jul 25, 2017
+
+@author: reischt1
+'''
+
 import sys
+sys.path.append('/u/58/reischt1/unix/ml_projects/MutualInformation/src')
+from MutualInformation import pyMIestimator
 sys.path.append('/u/58/reischt1/unix/ml_projects/readAFM/src')
 import readAFMHDF5 as rHDF5
 
+k = 5
+n = 100
 
 db = rHDF5.AFMdata('/l/reischt1/toyDB_v14_twoAtoms3D.hdf5', [41,41,41,1])
 
-k = 5
 
-for sz in range(10, 30, 5):
-    for amp in [7.0, 10.0, 15.0]:
-        data = db.batch_runtimeSolution(200, sigmabasez = float(sz), amplificationFactor = amp, verbose = False)
+
+# for sz in range(2, 150):
+for sz in [200.]:
+    for amp in [10.0]:
+        try:
+            data = db.batch_runtimeSolution(n, sigmabasez = float(sz)/10., amplificationFactor = amp, verbose = True)
+        except KeyError:
+            print('KeyError occured. sz = {}, amp = {}'.format(sz, amp))
+            continue
         X = data['forces']
         Y = data['solutions']
-        
+        print("sz: %f, amp: %f"%(float(sz)/10., amp))
         I=pyMIestimator(X,Y,k)
-        print('I(sz= '+str(sz)+'; amp= '+str(amp)+') = '+str(I))
-        with open('Mutual_information-toyDB_v14-k'+str(k)+'.dat','a') as f:
-            f.write('I(sz= '+str(sz)+'; amp= '+str(amp)+') = '+str(I)+'\n')
+        print('I(sz= '+str(float(sz)/10.)+'; amp= '+str(amp)+') = '+str(I))
+        with open('Mutual_information-toyDB_v14-k'+str(k)+'_amp'+str(amp)+'_n'+str(n)+'.dat','a') as f:
+            f.write('sz= '+str(float(sz)/10.)+' amp= '+str(amp)+ ' I= '+str(I)+'\n')
